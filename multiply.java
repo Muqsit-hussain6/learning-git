9b0cad35ee42b42899261adc2139a1a87c1229f4
Muqsit <muqsithussain01@gmail.com>
2023-02-12 18:18:39 +0500

    Added multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+    public static void main(String args[])
+    {
+        int a = 4;
+        int b = 4;
+        System.out.println("multiplication is = "+(a*b));
+
+    }
+}
